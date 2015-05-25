966e361d48f40996eb7d8f5ad67be12f94f0544a
vrk37 <fightend7@gmail.com>
2015-05-25 15:30:21 +0530

    Changes

--- SampleProject/Controllers/v1ProductController.cs
+++ SampleProject/Controllers/v1ProductController.cs
@@ -35,132 +35,7 @@ namespace SampleProject.Controllers
             return db.v1Product;
         }
 
-        // GET: odata/v1Product(5)
-        [EnableQuery]
-        public SingleResult<v1Product> Getv1Product([FromODataUri] int key)
-        {
-            return SingleResult.Create(db.v1Product.Where(v1Product => v1Product.ProductID == key));
-        }
-
-        // PUT: odata/v1Product(5)
-        public IHttpActionResult Put([FromODataUri] int key, Delta<v1Product> patch)
-        {
-            Validate(patch.GetEntity());
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            v1Product v1Product = db.v1Product.Find(key);
-            if (v1Product == null)
-            {
-                return NotFound();
-            }
-
-            patch.Put(v1Product);
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!v1ProductExists(key))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return Updated(v1Product);
-        }
-
-        // POST: odata/v1Product
-        public IHttpActionResult Post(v1Product v1Product)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.v1Product.Add(v1Product);
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateException)
-            {
-                if (v1ProductExists(v1Product.ProductID))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return Created(v1Product);
-        }
-
-        // PATCH: odata/v1Product(5)
-        [AcceptVerbs("PATCH", "MERGE")]
-        public IHttpActionResult Patch([FromODataUri] int key, Delta<v1Product> patch)
-        {
-            Validate(patch.GetEntity());
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            v1Product v1Product = db.v1Product.Find(key);
-            if (v1Product == null)
-            {
-                return NotFound();
-            }
-
-            patch.Patch(v1Product);
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!v1ProductExists(key))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return Updated(v1Product);
-        }
-
-        // DELETE: odata/v1Product(5)
-        public IHttpActionResult Delete([FromODataUri] int key)
-        {
-            v1Product v1Product = db.v1Product.Find(key);
-            if (v1Product == null)
-            {
-                return NotFound();
-            }
-
-            db.v1Product.Remove(v1Product);
-            db.SaveChanges();
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
+ 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -168,11 +43,6 @@ namespace SampleProject.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        private bool v1ProductExists(int key)
-        {
-            return db.v1Product.Count(e => e.ProductID == key) > 0;
         }
     }
 }
